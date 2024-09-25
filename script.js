46918857ae1be000b64fa8e229b8b667dd0b1d70
RubaRashid <rubarashidkhan@gmail.com>
2024-09-26 01:04:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+var bioDara = {
+    name : "Ruba" ,
+    fName : "Rashid",
+    age : 20,
+    phoneNo : "0300-1234567",
+};
+
+var car = {
+    type:"Fiat",
+    model:"500", 
+    color:"white"
+};
+
+var mango = {
+    type:"Sindri",
+    price:"500", 
+    color:"Yellow",
+    taste: "sweet"
+};
+
+var mobile = {
+    company: "Oppo",
+    model:"Oppo A57", 
+    color:"white",
+    memory: "3GB, 32GB"
+};
+
+var house = {
+    rooms: 5,
+    color: "hash white",
+    price: "15000000",
+};
+
+var book = {
+    title: "1984",
+    name: "Bang-e-Dara",
+    author: "Sir Syed Ahmed Khan",
+    pages: 328,
+};
+
+var student = {
+    name: "Ruba",
+    grade: 14,
+    subjects: ['Math', 'Science', 'English'],
+    instituteName: "SBBD university"
+};
+
+
+var dog = {
+    name: "puppy",
+    breed: "Golden Retriever",
+    color: "Golden"
+};
+
+var bird = {
+    name: "Eagle",
+    eat: "meat",
+    color: "black and brown"
+};
+
+var recipe = {
+    title: "cakes",
+    ingredients: ['Flour', 'Milk', 'Eggs', 'Sugar'],
+    totalCost: 500
+};
+
